259a7a5ca2bb3f5fca14467c1ddcf47e326ae9f5
Hiromichi123 <154042836+Hiromichi123@users.noreply.github.com>
2024-08-17 12:12:28 +0800

    8.3 HashMap

--- 8.3 HashMap/src/main.rs
+++ 8.3 HashMap/src/main.rs
@@ -44,4 +44,20 @@ fn main() {
         // 通常遍历后还需要使用HashMap，所以使用引用来遍历
         println!("{}: {}", key, value);
     }
+
+    // 更新HashMap
+    // or_insert方法，如果键存在则返回对应值的可变引用，否则插入新值并返回值的可变引用
+    scores.entry(String::from("Blue")).or_insert(50);
+    // 为了方便，Rust可以不显示接收返回值
+
+    // 根据旧值更新新值
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+    // split_whitespace方法返回一个迭代器，将字符串按空格分割为单词
+    for word in text.split_whitespace() {
+        // or_insert方法返回一个可变引用，这里使用+=操作对应值
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:#?}", map);
 }
